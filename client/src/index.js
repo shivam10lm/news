21d2d90558b6988156d6a600b82fcfc541620edd
Shivam <shivamgpt34@gmail.com>
2024-10-31 15:20:01 -0600

    last

--- client/src/index.js
+++ client/src/index.js
@@ -2,26 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/main.css";
-import { createTheme, ThemeProvider } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#1976d2",
-    },
-    secondary: {
-      main: "#dc004e",
-    },
-  },
-});
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
